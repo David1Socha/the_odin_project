e0da78a4ab5cb7c0ba5334dab40740825ccc08fb
David Socha <David1Socha@gmail.com>
2014-08-26 12:29:19 -0500

    Fixed bug with determination of ties

--- tic_tac_toe/tictactoe.rb
+++ tic_tac_toe/tictactoe.rb
@@ -2,16 +2,20 @@ module TicTacToe
 
   class Grid
 
-  	attr_accessor :contents, :size
+    attr_accessor :contents, :size
+
+    def full? 
+      return @contents.all? { |e| e.none? { |f| f.is_a? Numeric } } 
+    end
 
     def initialize(size)
       @size = size
       @contents = Array.new(size) { Array.new(size) }
       @contents.map!.with_index do |row, i|
-      	row.map!.with_index do |value, j|
-      	  rowcol_to_num(i, j)
-      	end
-      end	
+        row.map!.with_index do |value, j|
+          rowcol_to_num(i, j)
+        end
+      end
     end
 
     def rowcol_to_num(row, col)
@@ -39,14 +43,14 @@ module TicTacToe
 
     def has_won_horizontal?(row, player)
       for j in 0...@size
-      	return false unless player.symbol == @contents[row][j]
+        return false unless player.symbol == @contents[row][j]
       end
       return true
     end
 
     def has_won_vertical?(col, player)
       for i in 0...@size
-      	return false unless player.symbol == @contents[i][col]
+        return false unless player.symbol == @contents[i][col]
       end
       return true
     end    
@@ -54,25 +58,29 @@ module TicTacToe
     def has_won_diagonal?(row, col, player) 
       return false unless row == col
       for i, j in (row-1).downto(0).zip((col-1).downto(0))
-      	break if i.nil? || j.nil?
-      	return false unless player.symbol == @contents[i][j]
+        break if i.nil? || j.nil?
+        puts 'a'
+        return false unless player.symbol == @contents[i][j]
       end
       for i, j in ((row+1)...@size).zip((col+1)...@size)
-      	break if i.nil? || j.nil?
-      	return false unless player.symbol == @contents[i][j]
+        break if i.nil? || j.nil?
+        puts 'b'
+        return false unless player.symbol == @contents[i][j]
       end
       return true
-    end       
+    end
 
     def has_won_antidiagonal?(row, col, player)
-      return false unless row == col
+      return false unless row + col == @size - 1
       for i, j in (row-1).downto(0).zip((col+1)...@size)
-      	break if i.nil? || j.nil?
-      	return false unless player.symbol == @contents[i][j]
+        break if i.nil? || j.nil?
+        puts 'c'
+        return false unless player.symbol == @contents[i][j]
       end
       for i, j in ((row+1)...@size).zip((col-1).downto(0))
-      	break if i.nil? || j.nil?
-      	return false unless player.symbol == @contents[i][j]
+        break if i.nil? || j.nil?
+        puts 'd'
+        return false unless player.symbol == @contents[i][j]
       end
       return true
     end
@@ -80,10 +88,10 @@ module TicTacToe
     def to_s
       output = String.new
       @contents.each do |row|
-      	row.each do |e|
-      	  output+= "#{e} "
-      	end
-      	output+= "\n"
+        row.each do |e|
+          output+= "#{e} "
+        end
+        output+= "\n"
       end
       output
     end
@@ -92,74 +100,75 @@ module TicTacToe
 
   class Player
 
-  	attr_accessor :symbol, :name
+    attr_accessor :symbol, :name
 
-  	def initialize(sym, name)
-  	  @symbol = sym
-  	  @name = name
-  	end
+    def initialize(sym, name)
+      @symbol = sym
+      @name = name
+    end
 
   end
 
   class Game
 
-  	def initialize(size = 3)
-  	  @grid = Grid.new(size)
-  	  p1 = Player.new(:X, "Player 1")
-  	  p2 = Player.new(:O, "Player 2")
-  	  @players = [p1, p2]
-  	end
+    def initialize(size = 3, players)
+      @grid = Grid.new(size)
+      @players = players
+    end
 
-  	def get_move(p)
-  	  valid_input = false
-  	  until valid_input
+    def get_move(p)
+      valid_input = false
+      until valid_input
         begin
-  	      puts "#{p.name}, choose where you would like to move."
-  	      n = Integer(gets.chomp)
-  	      row, col = @grid.num_to_rowcol(n)
-  	      if @grid.can_place_symbol?(row, col)
-  	        valid_input = true
-  	      else
-  	        puts "That position is already filled"
-  	      end
+          puts "#{p.name}, choose where you would like to move."
+          n = Integer(gets.chomp)
+          row, col = @grid.num_to_rowcol(n)
+          if @grid.can_place_symbol?(row, col)
+            valid_input = true
+          else
+            puts "That position is already filled"
+          end
         rescue ArgumentError
-  	      puts "Please enter an integer"
+          puts "Please enter an integer"
         rescue IndexError
-  	  	  puts "Number is outside of grid range."
+          puts "Number is outside of grid range."
         end
-  	  end
-  	  return row, col
-  	end
+      end
+      return row, col
+    end
 
-  	def play
-      game_won = false
-  	  until game_won
-  	    @players.each do |p|
-  	      puts @grid
-  	      row, col = get_move p
-  	      @grid.place_symbol(row, col, p.symbol)
-  	      if @grid.has_won?(row, col, p)
-  	      	display_victory p
-  	      	break
-  	      elsif @grid.full?
-  	        display_tie
-  	      end
-  	    end
-  	  end
-  	end
+    def play
+      loop do
+        @players.each do |p|
+          puts @grid
+          row, col = get_move p
+          @grid.place_symbol(row, col, p.symbol)
+          if @grid.has_won?(row, col, p)
+            display_victory p
+            return
+          elsif @grid.full?
+            display_tie
+            return
+          end
+        end
+      end
+    end
 
-  	def display_tie
-  	  puts "Tie game. Nobody wins"
-  	end
+    def display_tie 
+      puts "Tie game. Nobody wins"
+    end
 
-  	def display_victory(winning_player)
-  	  puts "#{winning_player.name} wins!"
-  	end
+    def display_victory(winning_player)
+      puts "#{winning_player.name} wins!"
+    end
 
   end
 
 end
 
 #To run a sample 3x3 game of TicTacToe
-game = TicTacToe::Game.new
+p1 = TicTacToe::Player.new(:X, "Player 1")
+p2 = TicTacToe::Player.new(:O, "Player 2")
+players = [p1, p2]
+game = TicTacToe::Game.new(players)
 game.play
