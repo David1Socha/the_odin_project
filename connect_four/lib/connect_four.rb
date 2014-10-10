774235277732883861757283c5943afe0b6cbe53
David Socha <David1Socha@gmail.com>
2014-10-10 12:00:32 -0500

    Added method for checking first empty row in a given column

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -4,4 +4,9 @@ class Board
   def initialize(row_len, col_len)
     @contents = Array.new (col_len) { Array.new(row_len) }
   end
+
+  def lowest_empty_row_index(col) #returns index of lowest row where specified column is empty
+    low_row_index = @contents.rindex { |row| row[col].nil?}
+  end
+
 end
--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -22,4 +22,22 @@ describe "Board" do
 
   end
 
+  describe "#lowest_empty_row" do
+
+    before(:all) do
+      @board = Board.new(7,8)
+    end
+
+    it "returns nil when entire column is full" do
+      @board.contents.each {|row| row[2] = :O}
+      expect(@board.lowest_empty_row_index(2)).to be_nil
+    end
+
+    it "returns lowest row index when multiple rows are empty in specified column" do
+      @board.contents[7][5] = :X
+      expect(@board.lowest_empty_row_index(5)).to eq(6)
+    end
+
+  end
+
 end
