3b4875eb0945201ca53069f074b2c5658d1b8938
David Socha <David1Socha@gmail.com>
2014-10-12 02:24:51 -0500

    added has_won_vertical method

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -1,7 +1,10 @@
 class Board
+  CONNECT_LENGTH = 4
   attr_accessor :contents
 
   def initialize(row_len, col_len)
+    @rows = col_len
+    @cols = row_len
     @contents = Array.new (col_len) { Array.new(row_len) }
   end
 
@@ -12,6 +15,15 @@ class Board
   def place_symbol(col, symbol)
     row = lowest_empty_row_index(col)
     @contents[row][col] = symbol unless row.nil?
+    return row, col
+  end
+
+  def has_won_vertical?(row, col, symbol)
+    return false if row > @rows - CONNECT_LENGTH #need enough elements for connection to exist
+    CONNECT_LENGTH.times do |offset|
+      return false if @contents[row+offset][col] != symbol
+    end
+    return true
   end
 
 end
--- connect_four/spec/connect_four_spec.rb
+++ connect_four/spec/connect_four_spec.rb
@@ -5,7 +5,7 @@ describe "Board" do
   describe "#new" do
 
     before(:all) do
-      @board = Board.new(7,8)
+      @board = Board.new(3,2)
     end
 
     it "returns a new board object" do
@@ -13,11 +13,11 @@ describe "Board" do
     end
 
     it "creates rows of specified length" do
-      expect(@board.contents[0].size).to eq 7
+      expect(@board.contents[0].size).to eq 3
     end
 
     it "creates columns of specified length" do
-      expect(@board.contents.size).to eq 8
+      expect(@board.contents.size).to eq 2
     end
 
   end
@@ -25,7 +25,7 @@ describe "Board" do
   describe "#lowest_empty_row_index" do
 
     before(:each) do
-      @board = Board.new(7,8)
+      @board = Board.new(6,7)
     end
 
     it "returns nil when entire column is full" do
@@ -34,8 +34,8 @@ describe "Board" do
     end
 
     it "returns lowest row index when multiple rows are empty in specified column" do
-      @board.contents[7][5] = :X
-      expect(@board.lowest_empty_row_index(5)).to eq(6)
+      @board.contents[6][5] = :X
+      expect(@board.lowest_empty_row_index(5)).to eq(5)
     end
 
   end
@@ -43,13 +43,13 @@ describe "Board" do
   describe "#place_symbol" do
 
     before(:each) do
-      @board = Board.new(7, 8)
+      @board = Board.new(6, 7)
     end
     
     it "adds specified symbol to lowest empty row in column" do
-      allow(@board).to receive(:lowest_empty_row_index).and_return(7)
+      allow(@board).to receive(:lowest_empty_row_index).and_return(6)
       @board.place_symbol(1, :O)
-      expect(@board.contents[7][1]).to eq(:O)
+      expect(@board.contents[6][1]).to eq(:O)
     end
 
     it "does nothing if entire column is full" do
@@ -57,6 +57,37 @@ describe "Board" do
       old_contents = Array.new(@board.contents)
       @board.place_symbol(0, :X)
       expect(@board.contents).to eq(old_contents)
+    end
+
+  end
+
+  describe "#has_won_vertical?" do
+
+    before(:each) do
+      @board = Board.new(6, 7)
+    end
+
+    it "returns true when four consecutive vertical positions contain specified symbol" do
+      @board.contents[3][2] = :Q
+      @board.contents[4][2] = :Q
+      @board.contents[5][2] = :Q
+      @board.contents[6][2] = :Q
+      expect(@board.has_won_vertical?(3,2,:Q)).to be true
+    end
+
+    it "returns false when there are not enough elements below last-placed symbol for connection to be formed" do
+      @board.contents[4][4] = :X
+      @board.contents[5][4] = :X
+      @board.contents[6][4] = :X
+      expect(@board.has_won_vertical?(4,4, :X)).to be false
+    end
+
+    it "returns false when fewer than four consecutive vertical positions contain specified symbol" do
+      @board.contents[3][2] = :P
+      @board.contents[4][2] = :Q
+      @board.contents[5][2] = :Q
+      @board.contents[6][2] = :Q
+      expect(@board.has_won_vertical?(3,2,:P)).to be false
     end
 
   end
