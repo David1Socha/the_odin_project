9c9bcf2b5652fe98fb38425bc4b89dfd69f7350c
David Socha <David1Socha@gmail.com>
2014-10-24 20:22:56 -0500

    Added input validation tests to HumanPlayer#get_move

--- connect_four/spec/human_player_spec.rb
+++ connect_four/spec/human_player_spec.rb
@@ -13,24 +13,43 @@ describe "HumanPlayer" do
 
   describe "#get_move" do
 
-    before(:all) do
+    before(:each) do
       @board = Board.new(7,6)
+      allow(@player).to receive(:puts)
     end
 
     it "Prompts the user for input" do
-      expect(@player).to receive(:puts).once
+      expect(@player).to receive(:puts)
       expect(@player).to receive(:gets).and_return("5")
       @player.get_move @board
     end
 
     it "Returns integer form of input if it is valid" do
-      allow(@player).to receive(:puts)
       expect(@player).to receive(:gets).and_return("2")
       expect(@player.get_move(@board)).to eq(2)
     end
 
     it "Repeats until input is valid number" do
       expect(@player).to receive(:gets).twice.and_return("asdf","4")
+      @player.get_move @board
+    end
+
+    it "Repeats if input is too small" do
+      expect(@player).to receive(:gets).exactly(3).times.and_return("-1239","-1","0")
+      @player.get_move @board
+    end
+
+    it "Repeats if input is too large" do
+      expect(@player).to receive(:gets).exactly(3).times.and_return("89319","7","6")
+      @player.get_move @board
+    end
+
+    it "Repeats if specified column is full" do
+      @board.contents.map! do |row| 
+        row[1] = :R
+        row
+      end
+      expect(@player).to receive(:gets).twice.and_return("1","4")
       @player.get_move @board
     end
 
